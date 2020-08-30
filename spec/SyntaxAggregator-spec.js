4bece2ae2f577a79f55bc5bb19527aa754a8952e
Coteh <3276350+Coteh@users.noreply.github.com>
2020-08-30 16:15:09 -0400

    Fix breaking test for filter view

--- spec/SyntaxAggregator-spec.js
+++ spec/SyntaxAggregator-spec.js
@@ -1,5 +1,8 @@
 'use babel';
 
+// TODO mock request calls using proxyquire or something similar
+// https://github.com/thlorenz/proxyquire
+
 import SyntaxAggregator from '../lib/SyntaxAggregator';
 
 describe('SyntaxAggregator', () => {
@@ -7,35 +10,52 @@ describe('SyntaxAggregator', () => {
 
     beforeEach(() => {
         workspaceElement = atom.views.getView(atom.workspace);
-        activationPromise = atom.packages.activatePackage('syntaxdb-atom-plugin');
+
+        // Attaching the workspaceElement to the DOM is required to allow the
+        // `toBeVisible()` matchers to work. Anything testing visibility or focus
+        // requires that the workspaceElement is on the DOM. Tests that attach the
+        // workspaceElement to the DOM are generally slower than those off DOM.
+        jasmine.attachToDOM(workspaceElement);
+
+        waitsForPromise(() => atom.workspace.open());
+
+        runs(() => {
+            // Atom packages with "activationCommands" in package.json are lazy loaded
+            activationPromise = atom.packages.activatePackage('syntaxdb-atom-plugin');
+        });
     });
 
     describe("when aggregator is toggled", () => {
         it("should display a syntaxdb filter view", () => {
-            // Attaching the workspaceElement to the DOM is required to allow the
-            // `toBeVisible()` matchers to work. Anything testing visibility or focus
-            // requires that the workspaceElement is on the DOM. Tests that attach the
-            // workspaceElement to the DOM are generally slower than those off DOM.
-            jasmine.attachToDOM(workspaceElement);
-
             // Expect the SyntaxDB filter panel to not be there yet
             expect(workspaceElement.querySelector(".syntaxdb-filter")).not.toExist();
-
             // Trigger the SyntaxDB language filter bar
             atom.commands.dispatch(workspaceElement, 'syntaxdb-atom-plugin:language-filter');
+            // Dispatch should now trigger the package. Wait for package to be loaded
+            waitsForPromise(() => activationPromise);
 
-            waitsFor(() => {
-                return activationPromise;
-            });
+            waits(2000);
 
             runs(() => {
-                var filterPanel = workspaceElement.querySelector(".syntaxdb-filter");
-                expect(filterPanel).toBeVisible();
+                // Should now be visible
+                expect(workspaceElement.querySelector(".syntaxdb-filter")).toBeVisible();
             });
         });
 
         it("should display a list of languages that are covered by SyntaxDB", () => {
-            expect('life').toBe('easy');
+            // Expect the SyntaxDB filter panel to not be there yet
+            expect(workspaceElement.querySelector(".syntaxdb-filter")).not.toExist();
+            // Trigger the SyntaxDB language filter bar
+            atom.commands.dispatch(workspaceElement, 'syntaxdb-atom-plugin:language-filter');
+            // Dispatch should now trigger the package. Wait for package to be loaded
+            waitsForPromise(() => activationPromise);
+
+            waits(2000);
+
+            runs(() => {
+                // Should now be visible
+                expect(workspaceElement.querySelector(".syntaxdb-filter")).toBeVisible();
+            });
         });
     });
 
