efa2c5658f4495e5b10d9b426dcea60d056478f9
James Cote <3276350+Coteh@users.noreply.github.com>
2020-08-31 23:53:41 -0400

    Add prettier (#31)
    * Add prettier
    
    * Add prettier override for yml

--- spec/SyntaxUtil-spec.js
+++ spec/SyntaxUtil-spec.js
@@ -1,20 +1,20 @@
 'use babel';
 
-import SyntaxUtil from "../lib/SyntaxUtil";
+import SyntaxUtil from '../lib/SyntaxUtil';
 
-describe("SyntaxUtil", () => {
-    describe("when newLineizeEditorText util func is used", () => {
-        it("should append a newline to pieces of text without a newline at the end", () => {
-            var beforeText = "This text has no newline.";
-            var expectedText = "This text has no newline.\n";
+describe('SyntaxUtil', () => {
+    describe('when newLineizeEditorText util func is used', () => {
+        it('should append a newline to pieces of text without a newline at the end', () => {
+            var beforeText = 'This text has no newline.';
+            var expectedText = 'This text has no newline.\n';
             var afterText = SyntaxUtil.newLineizeEditorText(beforeText);
 
             expect(afterText).toBe(expectedText);
         });
 
-        it("should NOT append a newline to pieces of text with a newline at the end", () => {
-            var beforeText = "This text has a newline.\n";
-            var expectedText = "This text has a newline.\n";
+        it('should NOT append a newline to pieces of text with a newline at the end', () => {
+            var beforeText = 'This text has a newline.\n';
+            var expectedText = 'This text has a newline.\n';
             var afterText = SyntaxUtil.newLineizeEditorText(beforeText);
 
             expect(afterText).toBe(expectedText);
