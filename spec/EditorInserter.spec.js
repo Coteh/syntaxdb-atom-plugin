98c686cb7c0c64a9efa81047276aae95e6138c18
Coteh <3276350+Coteh@users.noreply.github.com>
2020-09-04 01:43:51 -0400

    Add EditorInserter spec and rename spec files

---
+++ spec/EditorInserter.spec.js
@@ -0,0 +1,23 @@
+'use babel';
+
+import EditorInserter from '../lib/editor/EditorInserter';
+
+describe('EditorInserter', () => {
+    describe('when text insertion performed', () => {
+        it('should invoke text editor insertion', () => {
+            throw new Error('Not implemented');
+        });
+        it('should throw error if text is empty', () => {
+            throw new Error('Not implemented');
+        });
+        it('should throw error if no editor open', () => {
+            throw new Error('Not implemented');
+        });
+        it('should insert into multiple selections', () => {
+            throw new Error('Not implemented');
+        });
+        it('should prepend the same spacing as the first line to all other lines', () => {
+            throw new Error('Not implemented');
+        });
+    });
+});
