5466719d0a82880af1b7ab4b3c3d5459c4952b1e
James Cote <3276350+Coteh@users.noreply.github.com>
2020-10-01 23:08:20 -0400

    Add tests for ResultsPresenter (#65)
    * Add ResultsPresenter spec
    
    * Add test for presenting undefined result and remove exception when no result provided to hideResults
    
    * Add test case for showPanel not being called when no result provided to present
    
    * Inline stub instance creation call

--- spec/SyntaxAggregator-spec.js
+++ spec/SyntaxAggregator-spec.js
@@ -5,9 +5,9 @@ import SyntaxFilterView from '../lib/view/SyntaxFilterView';
 import SyntaxResultView from '../lib/view/SyntaxResultView';
 import { LanguageFilterMode } from '../lib/domain/SyntaxModes';
 import ResultsPresenter from '../lib/domain/ResultsPresenter';
-const sinon = require('sinon');
 const SyntaxAggregator = require('../lib/domain/SyntaxAggregator');
 const fs = require('fs');
+const sinon = require('sinon');
 const chai = require('chai');
 const { expect } = require('chai');
 chai.use(require('sinon-chai'));
@@ -233,7 +233,7 @@ describe('SyntaxAggregator', () => {
             language_permalink: 'csharp',
         };
 
-        it('should display concept', () => {
+        it('should present concept', () => {
             expect(resultsPresenter.showResults).to.not.have.been.called;
             syntaxAggregator.onSelect({
                 item: concept,
@@ -247,21 +247,6 @@ describe('SyntaxAggregator', () => {
                 },
                 resultView,
             );
-        });
-        it('should show panel for concept', () => {
-            // TODO move this to ResultsPresenter spec, since it tests whether presenter shows results panel
-            let syntaxAggregator = new SyntaxAggregator(new ResultsPresenter());
-            syntaxAggregator.setViews({
-                filterView: filterView,
-                resultView: resultView,
-            });
-
-            expect(resultView.showPanel).to.not.have.been.called;
-            syntaxAggregator.onSelect({
-                item: concept,
-                mode: LanguageFilterMode.SELECT_CONCEPT,
-            });
-            expect(resultView.showPanel).to.have.been.calledOnce;
         });
     });
 
@@ -286,51 +271,6 @@ describe('SyntaxAggregator', () => {
                 expect(() => syntaxAggregator.hide()).to.throw(
                     'No filter view provided',
                 );
-            });
-        });
-
-        describe("when result view isn't provided", () => {
-            let concept = {
-                id: 210,
-                concept_name: 'Variable Declaration',
-                category_id: 25,
-                position: 8,
-                language_id: 4,
-                concept_search: 'Variable Declaration in C#',
-                concept_permalink: 'variable-dec',
-                description:
-                    'Used to declare a variable. Variables can be implicitly or explicitly typed.\r\n\r\nVariables declared this way (without a static modifier) within classes are called instance variables. They belong to an instance of the class (i.e. an object).',
-                syntax:
-                    'modifier dataType variableName; ///modifier is optional\r\n\r\n///variables can be assigned values either separately or on declaration\r\nvariableName = value; ///separate line assignment\r\n\r\nmodifier dataType variableName = value; ///same line assignment\r\n\r\n///implicitly typed variable\r\nvar variable2 = value;',
-                notes:
-                    'The modifier (public, private) permits or restricts direct access to the variable with respect to its scope (class, method, etc.). Variables declared within a class are called fields.\r\n\r\nVariables without a modifier are known as local variables, typically used within a method. They are temporary and only exist within the scope of the where its declared method.\r\n\r\ndataType is the data type of the variable. ',
-                example:
-                    'public class Car { \r\n    private int speed; ///private variable declaration\r\n    public int wheels; ///public variable declaration\r\n    \r\n    /*...constructor, etc...*/\r\n\r\n    public void speedUp() {\r\n        ///local variable declaration, in line assignment, only seen within speedUp method\r\n        int speedIncrease = 10;\r\n        speed += speedIncrease;\r\n    }\r\n}',
-                keywords: 'fields',
-                related:
-                    '<a href="/ref/csharp/access-mod">Access Modifiers</a>\r\n<a href="/ref/csharp/data-types">Primitive Data Types</a>',
-                documentation:
-                    '<a href="https://msdn.microsoft.com/en-us/library/ms173104.aspx">Types (C# Programming Reference) - MSDN</a>',
-                language_permalink: 'csharp',
-            };
-
-            beforeEach(() => {
-                // TODO move this to ResultsPresenter spec, since it tests whether presenter throws when showing nonexistent results panel
-                syntaxAggregator = new SyntaxAggregator(new ResultsPresenter());
-                filterView = sinon.createStubInstance(SyntaxFilterView);
-                syntaxAggregator.setViews({
-                    filterView: filterView,
-                });
-                expect(syntaxAggregator.resultView).to.not.exist;
-            });
-
-            it('should throw exception if attempting to present results', () => {
-                expect(() =>
-                    syntaxAggregator.onSelect({
-                        item: concept,
-                        mode: LanguageFilterMode.SELECT_CONCEPT,
-                    }),
-                ).to.throw('No result view provided');
             });
         });
     });
