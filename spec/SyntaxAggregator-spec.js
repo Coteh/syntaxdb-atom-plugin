213f1e332593bde2bce1277312a4248993ef5186
James Cote <3276350+Coteh@users.noreply.github.com>
2020-10-09 00:23:28 -0400

    Add error handling to search and syntax filter modals (#64)
    * Initial implementation of error handling
    
    * Unlock if search ran into error
    
    * Display no internet message on search view instead of filter view
    
    * Add color to error messages and clear result box if no results could be loaded
    
    * Get rid of ugly _this variables
    
    * Implement tests for search errors + add missing test for search result selection

--- spec/SyntaxAggregator-spec.js
+++ spec/SyntaxAggregator-spec.js
@@ -274,4 +274,183 @@ describe('SyntaxAggregator', () => {
             });
         });
     });
+
+    describe("when there's an error performing request to SyntaxDB API", () => {
+        let getStub;
+
+        beforeEach(() => {
+            getStub = sinon.stub(request, 'get');
+        });
+        describe("when there's no internet connection", () => {
+            it('should display a message saying no internet connection', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.showPanel).to.not.have.been.called;
+                expect(filterView.setErrorMessage).to.not.have.been.called;
+
+                syntaxAggregator.toggle();
+                getStub.yield(
+                    {
+                        code: 'ENOTFOUND',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+                expect(filterView.showPanel).to.have.been.calledOnce;
+                expect(filterView.setErrorMessage).to.have.been.calledOnceWith(
+                    sinon.match('No internet connection'),
+                );
+            });
+        });
+        describe("when there's an unknown error", () => {
+            it('should display a message saying there was an error', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.showPanel).to.not.have.been.called;
+                expect(filterView.setErrorMessage).to.not.have.been.called;
+
+                syntaxAggregator.toggle();
+                getStub.yield(
+                    {
+                        code: 'ESOMEERROR',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+                expect(filterView.showPanel).to.have.been.calledOnce;
+                expect(filterView.setErrorMessage).to.have.been.calledOnceWith(
+                    sinon.match('Error connecting to SyntaxDB'),
+                );
+            });
+        });
+        describe('when initially opening the filter', () => {
+            it('should not show any results', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.setItems).to.not.have.been.called;
+
+                syntaxAggregator.toggle();
+                getStub.yield(
+                    {
+                        code: 'ESOMEERROR',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+                expect(filterView.showPanel).to.have.been.calledOnce;
+
+                expect(filterView.setItems).to.have.been.calledWith([]);
+            });
+        });
+        describe('when selecting language result item', () => {
+            it('should not alter the results list', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.setItems).to.not.have.been.called;
+                syntaxAggregator.currentMode =
+                    LanguageFilterMode.SELECT_LANGUAGE;
+
+                syntaxAggregator.onSelect({
+                    item: {
+                        language_permalink: 'csharp',
+                    },
+                    mode: LanguageFilterMode.SELECT_LANGUAGE,
+                });
+                getStub.yield(
+                    {
+                        code: 'ESOMEERROR',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+
+                expect(filterView.setItems).to.not.have.been.called;
+            });
+        });
+        describe('when selecting category result item', () => {
+            it('should not alter the results list', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.setItems).to.not.have.been.called;
+                syntaxAggregator.currentMode =
+                    LanguageFilterMode.SELECT_CATEGORY;
+
+                syntaxAggregator.onSelect({
+                    item: {
+                        language_permalink: 'csharp',
+                        id: 25,
+                    },
+                    mode: LanguageFilterMode.SELECT_CATEGORY,
+                });
+                getStub.yield(
+                    {
+                        code: 'ESOMEERROR',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+
+                expect(filterView.setItems).to.not.have.been.called;
+            });
+        });
+        describe('when selecting concept result item', () => {
+            let concept = {
+                id: 210,
+                concept_name: 'Variable Declaration',
+                category_id: 25,
+                position: 8,
+                language_id: 4,
+                concept_search: 'Variable Declaration in C#',
+                concept_permalink: 'variable-dec',
+                description:
+                    'Used to declare a variable. Variables can be implicitly or explicitly typed.\r\n\r\nVariables declared this way (without a static modifier) within classes are called instance variables. They belong to an instance of the class (i.e. an object).',
+                syntax:
+                    'modifier dataType variableName; ///modifier is optional\r\n\r\n///variables can be assigned values either separately or on declaration\r\nvariableName = value; ///separate line assignment\r\n\r\nmodifier dataType variableName = value; ///same line assignment\r\n\r\n///implicitly typed variable\r\nvar variable2 = value;',
+                notes:
+                    'The modifier (public, private) permits or restricts direct access to the variable with respect to its scope (class, method, etc.). Variables declared within a class are called fields.\r\n\r\nVariables without a modifier are known as local variables, typically used within a method. They are temporary and only exist within the scope of the where its declared method.\r\n\r\ndataType is the data type of the variable. ',
+                example:
+                    'public class Car { \r\n    private int speed; ///private variable declaration\r\n    public int wheels; ///public variable declaration\r\n    \r\n    /*...constructor, etc...*/\r\n\r\n    public void speedUp() {\r\n        ///local variable declaration, in line assignment, only seen within speedUp method\r\n        int speedIncrease = 10;\r\n        speed += speedIncrease;\r\n    }\r\n}',
+                keywords: 'fields',
+                related:
+                    '<a href="/ref/csharp/access-mod">Access Modifiers</a>\r\n<a href="/ref/csharp/data-types">Primitive Data Types</a>',
+                documentation:
+                    '<a href="https://msdn.microsoft.com/en-us/library/ms173104.aspx">Types (C# Programming Reference) - MSDN</a>',
+                language_permalink: 'csharp',
+            };
+
+            it('should present the result', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.setItems).to.not.have.been.called;
+                expect(resultsPresenter.showResults).to.not.have.been.called;
+
+                syntaxAggregator.currentMode =
+                    LanguageFilterMode.SELECT_CONCEPT;
+
+                expect(resultsPresenter.showResults).to.not.have.been.called;
+                syntaxAggregator.onSelect({
+                    item: concept,
+                    mode: LanguageFilterMode.SELECT_CONCEPT,
+                });
+
+                expect(getStub).to.not.have.been.called;
+                expect(filterView.setItems).to.not.have.been.called;
+                expect(resultsPresenter.showResults).to.have.been.calledOnce;
+                expect(resultsPresenter.showResults).to.have.been.calledWith(
+                    {
+                        item: concept,
+                        mode: LanguageFilterMode.SELECT_CONCEPT,
+                    },
+                    resultView,
+                );
+            });
+        });
+        afterEach(() => {
+            getStub.restore();
+        });
+    });
 });
--- spec/SyntaxSearch-spec.js
+++ spec/SyntaxSearch-spec.js
@@ -130,6 +130,47 @@ describe('SyntaxSearch', () => {
         });
     });
 
+    describe('when search result selected', () => {
+        let concept = {
+            id: 210,
+            concept_name: 'Variable Declaration',
+            category_id: 25,
+            position: 8,
+            language_id: 4,
+            concept_search: 'Variable Declaration in C#',
+            concept_permalink: 'variable-dec',
+            description:
+                'Used to declare a variable. Variables can be implicitly or explicitly typed.\r\n\r\nVariables declared this way (without a static modifier) within classes are called instance variables. They belong to an instance of the class (i.e. an object).',
+            syntax:
+                'modifier dataType variableName; ///modifier is optional\r\n\r\n///variables can be assigned values either separately or on declaration\r\nvariableName = value; ///separate line assignment\r\n\r\nmodifier dataType variableName = value; ///same line assignment\r\n\r\n///implicitly typed variable\r\nvar variable2 = value;',
+            notes:
+                'The modifier (public, private) permits or restricts direct access to the variable with respect to its scope (class, method, etc.). Variables declared within a class are called fields.\r\n\r\nVariables without a modifier are known as local variables, typically used within a method. They are temporary and only exist within the scope of the where its declared method.\r\n\r\ndataType is the data type of the variable. ',
+            example:
+                'public class Car { \r\n    private int speed; ///private variable declaration\r\n    public int wheels; ///public variable declaration\r\n    \r\n    /*...constructor, etc...*/\r\n\r\n    public void speedUp() {\r\n        ///local variable declaration, in line assignment, only seen within speedUp method\r\n        int speedIncrease = 10;\r\n        speed += speedIncrease;\r\n    }\r\n}',
+            keywords: 'fields',
+            related:
+                '<a href="/ref/csharp/access-mod">Access Modifiers</a>\r\n<a href="/ref/csharp/data-types">Primitive Data Types</a>',
+            documentation:
+                '<a href="https://msdn.microsoft.com/en-us/library/ms173104.aspx">Types (C# Programming Reference) - MSDN</a>',
+            language_permalink: 'csharp',
+        };
+
+        it('should present result', () => {
+            expect(resultsPresenter.showResults).to.not.have.been.called;
+
+            syntaxSearch.selectSearchResult({
+                item: concept,
+            });
+
+            expect(resultsPresenter.showResults).to.have.been.calledWithExactly(
+                {
+                    item: concept,
+                },
+                resultView,
+            );
+        });
+    });
+
     describe("when views aren't provided", () => {
         beforeEach(() => {
             syntaxSearch = new SyntaxSearch();
@@ -169,6 +210,121 @@ describe('SyntaxSearch', () => {
                     'No filter view provided',
                 );
             });
+        });
+    });
+
+    describe("when there's an error performing request to SyntaxDB API", () => {
+        let getStub;
+
+        beforeEach(() => {
+            getStub = sinon.stub(request, 'get');
+        });
+        describe("when there's no internet connection", () => {
+            it('should display a message saying no internet connection', () => {
+                expect(getStub).to.not.have.been.called;
+
+                syntaxSearch.performSearch({
+                    searchText: 'for loop',
+                });
+                getStub.yield(
+                    {
+                        code: 'ENOTFOUND',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+                expect(searchView.setErrorMessage).to.have.been.calledOnceWith(
+                    sinon.match('No internet connection'),
+                );
+            });
+        });
+        describe("when there's an unknown error", () => {
+            it('should display a message saying there was an error', () => {
+                expect(getStub).to.not.have.been.called;
+
+                syntaxSearch.performSearch({
+                    searchText: 'for loop',
+                });
+                getStub.yield(
+                    {
+                        code: 'ESOMEERROR',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+                expect(searchView.setErrorMessage).to.have.been.calledOnceWith(
+                    sinon.match('Error connecting to SyntaxDB'),
+                );
+            });
+        });
+        describe('when performing a search', () => {
+            it('should not show the search results view', () => {
+                expect(getStub).to.not.have.been.called;
+
+                syntaxSearch.performSearch({
+                    searchText: 'for loop',
+                });
+                getStub.yield(
+                    {
+                        code: 'ESOMEERROR',
+                    },
+                    null,
+                    null,
+                );
+
+                expect(getStub).to.have.been.calledOnce;
+                expect(filterView.showPanel).to.not.have.been.called;
+            });
+        });
+        describe('when selecting concept result item', () => {
+            let concept = {
+                id: 210,
+                concept_name: 'Variable Declaration',
+                category_id: 25,
+                position: 8,
+                language_id: 4,
+                concept_search: 'Variable Declaration in C#',
+                concept_permalink: 'variable-dec',
+                description:
+                    'Used to declare a variable. Variables can be implicitly or explicitly typed.\r\n\r\nVariables declared this way (without a static modifier) within classes are called instance variables. They belong to an instance of the class (i.e. an object).',
+                syntax:
+                    'modifier dataType variableName; ///modifier is optional\r\n\r\n///variables can be assigned values either separately or on declaration\r\nvariableName = value; ///separate line assignment\r\n\r\nmodifier dataType variableName = value; ///same line assignment\r\n\r\n///implicitly typed variable\r\nvar variable2 = value;',
+                notes:
+                    'The modifier (public, private) permits or restricts direct access to the variable with respect to its scope (class, method, etc.). Variables declared within a class are called fields.\r\n\r\nVariables without a modifier are known as local variables, typically used within a method. They are temporary and only exist within the scope of the where its declared method.\r\n\r\ndataType is the data type of the variable. ',
+                example:
+                    'public class Car { \r\n    private int speed; ///private variable declaration\r\n    public int wheels; ///public variable declaration\r\n    \r\n    /*...constructor, etc...*/\r\n\r\n    public void speedUp() {\r\n        ///local variable declaration, in line assignment, only seen within speedUp method\r\n        int speedIncrease = 10;\r\n        speed += speedIncrease;\r\n    }\r\n}',
+                keywords: 'fields',
+                related:
+                    '<a href="/ref/csharp/access-mod">Access Modifiers</a>\r\n<a href="/ref/csharp/data-types">Primitive Data Types</a>',
+                documentation:
+                    '<a href="https://msdn.microsoft.com/en-us/library/ms173104.aspx">Types (C# Programming Reference) - MSDN</a>',
+                language_permalink: 'csharp',
+            };
+
+            it('should present the result', () => {
+                expect(getStub).to.not.have.been.called;
+                expect(resultsPresenter.showResults).to.not.have.been.called;
+
+                syntaxSearch.selectSearchResult({
+                    item: concept,
+                });
+
+                expect(
+                    resultsPresenter.showResults,
+                ).to.have.been.calledWithExactly(
+                    {
+                        item: concept,
+                    },
+                    resultView,
+                );
+            });
+        });
+        afterEach(() => {
+            getStub.restore();
         });
     });
 });
